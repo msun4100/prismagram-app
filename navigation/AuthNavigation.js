b56b159aaffc6c2c042b8fe596ea22adfa7b97a2
msun4100 <msun4100@gmail.com>
2020-04-22 21:00:53 +0900

    #11.1 AuthNavigation

---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,22 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Signup from "../screens/Auth/Signup";
+import Confirm from "../screens/Auth/Confirm";
+import Login from "../screens/Auth/Login";
+import AuthHome from "../screens/Auth/AuthHome";
+
+const AuthNavigation = createStackNavigator();
+export default () => {
+  return (
+    <NavigationContainer>
+      <AuthNavigation.Navigator initialRouteName="AuthHome" headerMode="none">
+        <AuthNavigation.Screen name="AuthHome" component={AuthHome} />
+        <AuthNavigation.Screen name="Signup" component={Signup} />
+        <AuthNavigation.Screen name="Login" component={Login} />
+        <AuthNavigation.Screen name="Confirm" component={Confirm} />
+      </AuthNavigation.Navigator>
+    </NavigationContainer>
+  );
+};
