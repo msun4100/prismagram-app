98fb249c8b57bed87de37bbf70e6733d843743b4
msun4100 <msun4100@gmail.com>
2020-04-28 17:23:09 +0900

    #15.2 Post Component, Swiper

---
+++ components/Post.js
@@ -0,0 +1,161 @@
+import React from "react";
+import { Image, View, Platform } from "react-native/";
+import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
+import Swiper from "react-native-swiper";
+import constants from "../constants";
+import styles from "../styles";
+
+const Container = styled.View``;
+const Header = styled.View`
+  padding: 15px;
+  flex-direction: row;
+  align-items: center;
+`;
+const Touchable = styled.TouchableOpacity``;
+const HeaderUserContainer = styled.View`
+  margin-left: 10px;
+`;
+const Bold = styled.Text`
+  font-weight: 500;
+`;
+const Location = styled.Text`
+  font-size: 12px;
+`;
+
+const IconsContainer = styled.View`
+  flex-direction: row;
+  margin-bottom: 5px;
+`;
+const IconContainer = styled.View`
+  margin-right: 10px;
+`;
+
+const InfoContainer = styled.View`
+  padding: 10px;
+`;
+const Caption = styled.Text`
+  margin: 3px 0px;
+`;
+const CommentCount = styled.Text`
+  opacity: 0.6;
+  font-size: 13px;
+`;
+
+const Post = ({
+  user,
+  location,
+  files = [],
+  likeCount,
+  caption,
+  comments = [],
+}) => {
+  const SWIPER_HEIGHT = constants.height / 2.5;
+  const DEFAULT_DOT_STYLES = {
+    backgroundColor: "rgba(0,0,0,.2)",
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginLeft: 3,
+    marginRight: 3,
+    marginTop: 3,
+    marginBottom: 3,
+  };
+  return (
+    <Container>
+      <Header>
+        <Touchable>
+          <Image
+            style={{ height: 40, width: 40, borderRadius: 20 }}
+            source={{ uri: user.avatar }}
+          />
+        </Touchable>
+        <Touchable>
+          <HeaderUserContainer>
+            <Bold>{user.username}</Bold>
+            <Location>{location}</Location>
+          </HeaderUserContainer>
+        </Touchable>
+      </Header>
+      <Swiper
+        style={{ height: SWIPER_HEIGHT }}
+        showsButtons={false}
+        paginationStyle={{ position: "absolute", bottom: 5 }}
+        dot={<View style={{ ...DEFAULT_DOT_STYLES }} />}
+        activeDotColor={`${styles.lightGreyColor}`}
+      >
+        {files?.map((file) => (
+          <Image
+            style={{ width: constants.width, height: SWIPER_HEIGHT }}
+            key={file.id}
+            source={{ uri: file.url }}
+          />
+        ))}
+      </Swiper>
+      <InfoContainer>
+        <IconsContainer>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                size={28}
+                name={
+                  Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"
+                }
+              />
+            </IconContainer>
+          </Touchable>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                size={28}
+                name={Platform.OS === "ios" ? "ios-text" : "md-text"}
+              />
+            </IconContainer>
+          </Touchable>
+        </IconsContainer>
+        <Touchable>
+          <Bold>{likeCount === 1 ? "1 like" : `${likeCount} likes`}</Bold>
+        </Touchable>
+        <Caption>
+          <Bold>{user.username}</Bold> {caption}
+        </Caption>
+        <Touchable>
+          <CommentCount>See all {comments.length} comments</CommentCount>
+        </Touchable>
+      </InfoContainer>
+    </Container>
+  );
+};
+
+Post.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  createdAt: PropTypes.string.isRequired,
+};
+
+export default Post;
--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -5,6 +5,7 @@ import styles from "../../styles";
 import Loader from "../../components/Loader";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
+import Post from "../../components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -65,7 +66,11 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      {loading ? <Loader /> : <Text>Hello</Text>}
+      {loading ? (
+        <Loader />
+      ) : (
+        data?.seeFeed?.map((post) => <Post key={post.id} {...post} />)
+      )}
     </ScrollView>
   );
 };
