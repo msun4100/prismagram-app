dbcc71212f09065a4a38e625539657263c9e54c6
msun4100 <msun4100@gmail.com>
2020-05-06 22:22:28 +0900

    #18.5 Saving Photo

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { TouchableOpacity, Platform } from "react-native";
 import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
 import { Camera } from "expo-camera";
 import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components";
@@ -10,12 +11,41 @@ import styles from "../../styles";
 
 const View = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.View``;
+
+const Button = styled.View`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  border: 10px solid ${styles.lightGreyColor};
 `;
 
 export default ({ navigation }) => {
+  const cameraRef = useRef();
+  const [canTakePhoto, setCanTakePhoto] = useState(true);
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const takePhoto = async () => {
+    if (!canTakePhoto) {
+      return;
+    }
+    try {
+      setCanTakePhoto(false); // 한번만 사진찍도록
+      const { uri } = await cameraRef.current.takePictureAsync({
+        quality: 1,
+      });
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      console.log("asset", asset);
+    } catch (error) {
+      console.log(error);
+      setCanTakePhoto(true);
+    }
+  };
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -28,10 +58,10 @@ export default ({ navigation }) => {
     }
   };
   const toggleType = () => {
-    if (cameraType === Camera.Constants.type.front) {
-      setCameraType(Camera.Constants.type.back);
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
     } else {
-      setCameraType(Camera.Constants.type.front);
+      setCameraType(Camera.Constants.Type.front);
     }
   };
   useEffect(() => {
@@ -42,27 +72,35 @@ export default ({ navigation }) => {
       {loading ? (
         <Loader />
       ) : hasPermission ? (
-        <Camera
-          type={cameraType}
-          style={{
-            justifyContent: "flex-end",
-            padding: 20,
-            width: constants.width,
-            height: constants.height / 2,
-          }}
-        >
-          <TouchableOpacity onPress={toggleType}>
-            <Ionicons
-              name={
-                Platform.OS === "ios"
-                  ? "ios-reverse-camera"
-                  : "md-reverse-camera"
-              }
-              size={28}
-              color={styles.blackColor}
-            />
-          </TouchableOpacity>
-        </Camera>
+        <>
+          <Camera
+            ref={cameraRef}
+            type={cameraType}
+            style={{
+              justifyContent: "flex-end",
+              padding: 20,
+              width: constants.width,
+              height: constants.height / 2,
+            }}
+          >
+            <TouchableOpacity onPress={toggleType}>
+              <Ionicons
+                name={
+                  Platform.OS === "ios"
+                    ? "ios-reverse-camera"
+                    : "md-reverse-camera"
+                }
+                size={28}
+                color={styles.screenBackgroundColor}
+              />
+            </TouchableOpacity>
+          </Camera>
+          <View>
+            <TouchableOpacity onPress={takePhoto} disabled={!canTakePhoto}>
+              <Button />
+            </TouchableOpacity>
+          </View>
+        </>
       ) : null}
     </View>
   );
